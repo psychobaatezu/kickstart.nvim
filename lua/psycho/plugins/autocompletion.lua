bf956bb3a39d239b7adfc5bcb4a5b2fa81cf118f
Fred Allard <frederic.allard@goto.com>
2024-10-13 14:27:28 -0400

    disable lsp autocomplettion only

--- lua/psycho/plugins/autocompletion.lua
+++ lua/psycho/plugins/autocompletion.lua
@@ -47,7 +47,11 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
-        completion = { completeopt = 'menu,menuone,noinsert' },
+        completion = {
+          completeopt = 'menu,menuone,noinsert',
+          keyword_length = 100,
+          autocomplete = false,
+        },
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
